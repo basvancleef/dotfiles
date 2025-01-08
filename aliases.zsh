5295d68593bc8b5281699487820992115809db18
Bas van Cleef <basvancleef@icloud.com>
2025-01-08 11:26:38 +0100

    aliases: Seperate utility functions from aliases

--- aliases.zsh
+++ aliases.zsh
@@ -43,33 +43,3 @@ alias accept="git checkout accept"
 alias develop="git checkout develop"
 alias remote="git remote -v"
 alias fixup="git commit --fixup"
-
-###########################################################################
-
-# Start dev build
-# Usage: `dev` or `dev dashboard`
-dev() {
-        _FILTER="$1"
-
-        if [ -f "pnpm-lock.yaml" ]; then
-                if [ -z "$_FILTER" ]; then
-                        pnpm dev
-                else
-                        pnpm --filter="*$1*" dev
-                fi
-        else
-                npm run dev
-        fi
-}
-
-# Open jira issue or project
-# Usage: `jira ABC-123` or `jira ABC` or just `jira`
-jira() {
-  ISSUE=$1
-  while [[ -z "$ISSUE" ]]
-  do
-    read "ISSUE?Jira key: "
-  done
-
-  open "https://bttr.atlassian.net/browse/$ISSUE"
-}
